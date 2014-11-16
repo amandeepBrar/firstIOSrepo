60c6a14045b8f296d9ebafda5a5049932dabfdbc
Omen Brar <OmenBrar@Omens-MacBook-Air.local>
2014-11-16 13:04:39 -0800

    Initial Commit

---
+++ Ios_table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ios_table
+//
+//  Created by Omen Brar on 11/16/14.
+//  Copyright (c) 2014 omenBrar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ios_table/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ios_table
+//
+//  Created by Omen Brar on 11/16/14.
+//  Copyright (c) 2014 omenBrar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
